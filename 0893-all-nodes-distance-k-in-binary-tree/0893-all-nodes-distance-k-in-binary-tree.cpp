f034b3c514d93b4f3d06dc020cadbb33f04a3ef9
Chaitanya Arora <127611582+chaitanyaa7@users.noreply.github.com>
2023-07-11 15:57:06 +0530

    Time: 7 ms (65.63%), Space: 13.2 MB (28.63%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -1,33 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
-    void markParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &parent_track, TreeNode* target) {
+public:
+   void markParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &parent, TreeNode* target) {
         queue<TreeNode*> queue;
         queue.push(root);
         while(!queue.empty()) { 
             TreeNode* current = queue.front(); 
             queue.pop();
             if(current->left) {
-                parent_track[current->left] = current;
+                parent[current->left] = current;
                 queue.push(current->left);
             }
             if(current->right) {
-                parent_track[current->right] = current;
+                parent[current->right] = current;
                 queue.push(current->right);
             }
         }
     }
-public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode*, TreeNode*> parent_track; // node -> parent
-        markParents(root, parent_track, target); 
-        
-        unordered_map<TreeNode*, bool> visited; 
+        unordered_map<TreeNode*,TreeNode*> parent;
+        markParents(root,parent,target);
         queue<TreeNode*> queue;
+
+        unordered_map<TreeNode*,bool> visited;
+        visited[target]=true;
         queue.push(target);
-        visited[target] = true;
-        int curr_level = 0;
-        while(!queue.empty()) { /*Second BFS to go upto K level from target node and using our hashtable info*/
-            int size = queue.size();
-            if(curr_level++ == k) break;
+        int cnt=0;
+        while(!queue.empty()){
+            if(cnt++==k)break;
+            int size=queue.size();
             for(int i=0; i<size; i++) {
                 TreeNode* current = queue.front(); queue.pop();
                 if(current->left && !visited[current->left]) {
@@ -38,17 +47,20 @@ public:
                     queue.push(current->right);
                     visited[current->right] = true;
                 }
-                if(parent_track[current] && !visited[parent_track[current]]) {
-                    queue.push(parent_track[current]);
-                    visited[parent_track[current]] = true;
+                if(parent[current] && !visited[parent[current]]) {
+                    queue.push(parent[current]);
+                    visited[parent[current]] = true;
                 }
             }
+            
+
+
         }
-        vector<int> result;
-        while(!queue.empty()) {
-            TreeNode* current = queue.front(); queue.pop();
-            result.push_back(current->val);
-        }
-        return result;
+        vector<int> ans;
+        while(!queue.empty()){
+            TreeNode* temp=queue.front();
+            queue.pop();
+            ans.push_back(temp->val);
+        }return ans;
     }
 };
