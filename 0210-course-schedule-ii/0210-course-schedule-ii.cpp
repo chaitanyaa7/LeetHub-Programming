48ffeda6abda1325390b3817dba94c8632f957fc
Chaitanya Arora <127611582+chaitanyaa7@users.noreply.github.com>
2023-07-14 01:21:08 +0530

    Time: 27 ms (43.57%), Space: 13.8 MB (63.1%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& prerequisites) {
+        vector<int> adj[V];
+	    for(auto it:prerequisites){
+	        adj[it[1]].push_back(it[0]);
+	    }
+	    queue<int> q;
+	    
+	    vector<int> in(V,0);
+	    for(int i=0;i<V;i++){
+	        for(auto it: adj[i]) in[it]++;
+	        
+	    }
+	    for(int i=0;i<V;i++){
+	        if(in[i]==0)q.push(i);
+	    }
+	   vector<int> ans;
+	    while(!q.empty()){
+	        int temp=q.front();
+	        q.pop();
+	        ans.push_back(temp);
+	        for(auto it: adj[temp]){
+	            in[it]--;
+	            if(in[it]==0){
+	                q.push(it); 
+	        }
+	    }
+	    }
+	     if(ans.size()==V) return ans;
+	    return {};
+    }
+};
