da64024224d2106801e72ba438f6e1d23e27fc22
Chaitanya Arora <127611582+chaitanyaa7@users.noreply.github.com>
2023-08-26 23:52:18 +0530

    Time: 106 ms (67.44%), Space: 41.9 MB (55.29%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -2,40 +2,52 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         
-        vector<vector<int>> adj[n+1];
-        for(int i=0;i<times.size();i++){
-                  int u=times[i][0];
-                  int v=times[i][1];
-                  int w=times[i][2];    
-                  adj[u].push_back({v,w});
-            }
+       unordered_map<int,list<pair<int,int>>> adj;
+        for(int i=0; i<times.size(); i++) {
+            int u = times[i][0];
+            int v = times[i][1];
+            int d = times[i][2];
+            adj[u].push_back({v,d});
+        }
         
+        //Dijkstra's Algo
         priority_queue< pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>> > pq;
+        
+        vector<int> distance(n+1, INT_MAX);
+        distance[0]=0;
+        
         pq.push({0,k});
-        vector<int>dist(n+1,1e9);
-        dist[k]=0,dist[0]=0;
-
-        while(!pq.empty()){
-            auto it= pq.top();
+        distance[k] = 0;
+        
+        while(!pq.empty())  {
+            
+            auto it = pq.top();
             pq.pop();
-            int node=it.second;
-            int cost=it.first;
-
-            for(auto itr: adj[node]){
-                 int adjN=itr[0];
-            int adjW=itr[1];
-             if(cost+adjW < dist[adjN]){
-                 dist[adjN]=cost+adjW;
-                 pq.push({cost+adjW,adjN});
-             }
+            int dist = it.first;
+            int node = it.second;
+            
+            for(auto it : adj[node])    {
+                int adjNode = it.first;
+                int wt = it.second;
+                
+                int curr = wt + dist;
+                
+                if(curr<distance[adjNode])  {
+                    distance[adjNode] = curr;
+                    pq.push({curr,adjNode});
+                }
             }
         }
-
-        int ans=-1;
-        for(int i=0;i<n+1;i++){
-            if(dist[i]==1e9)return -1;
-            ans=max(ans,dist[i]);
+        
+        int mx = -1;
+        for(int i=0; i<n+1; i++)  {
+            // cout<<i<<" = "<<distance[i]<<"\n";
+            if(distance[i]==INT_MAX)    {
+                return -1;
+            }
+            mx = max(distance[i],mx);
         }
-        return ans;
+        
+        return mx;
     }
 };
