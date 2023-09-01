2cbf80b9bb0d1cbeb6d5237053f5f74fad2d6c69
Chaitanya Arora <127611582+chaitanyaa7@users.noreply.github.com>
2023-09-01 12:12:31 +0530

    Time: 0 ms (100%), Space: 7.7 MB (89.73%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        ans[0]=0;
+        for(int i=1;i<=n;i++){
+              if(i%2) ans[i]=ans[i/2]+1;
+              else ans[i]=ans[i/2];
+        }
+        return ans;
+    }
+};
