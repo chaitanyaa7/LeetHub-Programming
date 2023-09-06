7608633028da9be499d7b03f7b2ceebb953bd8e4
Chaitanya Arora <127611582+chaitanyaa7@users.noreply.github.com>
2023-09-06 19:36:34 +0530

    Time: 4 ms (9.44%), Space: 6.6 MB (7.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -18,8 +18,8 @@ public:
                 if(i==0 && j==0) dp[i][j]=1;
                 else{
                     int up=0,left=0;
-                    if(i>0)up=f(i-1,j,dp);
-                    if(j>0) left=f(i,j-1,dp);
+                    if(i>0)up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
                     dp[i][j]=up+left;
                 }
             }
