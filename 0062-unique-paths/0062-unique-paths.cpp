bcb93810fde11e6254690bdb8420b09e26991174
Chaitanya Arora <127611582+chaitanyaa7@users.noreply.github.com>
2023-09-06 19:28:10 +0530

    Time: 3 ms (29.63%), Space: 6.6 MB (7.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -12,8 +12,19 @@ public:
     }
 
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,dp);
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0)up=f(i-1,j,dp);
+                    if(j>0) left=f(i,j-1,dp);
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
         
     }
 };
