44454e0bd62c4c45fc9b7077a9865b7c4dbfe771
Chaitanya Arora <127611582+chaitanyaa7@users.noreply.github.com>
2023-09-25 17:42:13 +0530

    Time: 2 ms (70.52%), Space: 7.3 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int arr[26]={0};
-        for(int i=0;i<s.size();i++){
-            arr[s[i]-'a']++;
-        }
-        for(int i=0;i<t.size();i++){
-            arr[t[i]-'a']--;
-            if(arr[t[i]-'a']==-1)return t[i];
-        }
-        return '\0';
+       char ans=0;
+       for(auto it:s+t){
+           ans^=it;
+       }
+       return ans;
         
 
     }
