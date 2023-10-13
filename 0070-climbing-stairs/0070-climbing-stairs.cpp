532413eda553398dfb22fa403d86ba462118c8ad
Chaitanya Arora <127611582+chaitanyaa7@users.noreply.github.com>
2023-10-13 08:16:03 +0530

    Time: 3 ms (10.41%), Space: 6.4 MB (24.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,6 +7,10 @@ public:
     }
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        return f(n,dp);
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
